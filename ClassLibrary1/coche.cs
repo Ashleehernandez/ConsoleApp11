d4a71a39ad7c54a970bdc0adb1c39ae671bd8464
ashle <ashle@DESKTOP-VVPHGA7>
2025-02-19 19:40:47 -0400

    Add project files.

---
+++ ClassLibrary1/coche.cs
@@ -0,0 +1,60 @@
+﻿namespace ClassLibrary1
+{
+    public class coche
+    {
+        private string Modelo { get; set; }
+        private int VelocidadMaxima { get; set; }
+        private int PotenciaMotor { get; set; }
+        private bool Enmarcha {  get; set; }
+
+
+        public coche(string modelo , int velocidamaxima , int potenciamotor , bool enmarcha )
+        {
+            this.Modelo = modelo;
+            this.VelocidadMaxima = velocidamaxima;
+            this.PotenciaMotor = potenciamotor;
+            this.Enmarcha = enmarcha;
+        }
+
+
+        public void Acelerar()
+        {
+            this.VelocidadMaxima += 10;
+            if (Enmarcha)
+            {
+                Console.WriteLine("el coche " + this.Modelo + "esta acelerando " + this.VelocidadMaxima+ "km/h");
+
+            }
+            else
+            {
+                Console.WriteLine("primero encienda el coche ");
+            }
+
+        }
+
+        public void Frenar()
+        {
+            VelocidadMaxima = 0;
+            Enmarcha = false;
+            Console.WriteLine("el coche esta frenando  " +this.Modelo);
+        }
+
+
+        public void Encender()
+        {
+            Enmarcha = true;
+            Console.WriteLine("El coche "+ this.Modelo+ "esta ensendido ");
+
+        }
+
+        public void Apagar()
+        {
+            Enmarcha = false ;
+            Console.WriteLine("el coche " +this.Modelo+ "esta apagado");
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+
+namespace ConsoleApp11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            coche cones = new coche("Honda cr-v" , 120, 1200 , false);
+
+
+            cones.Encender();
+            cones.Acelerar();
+            cones.Frenar();
+            cones.Apagar();
+
+            coche nana = new coche("bmw", 320, 3000, false);
+            nana.Encender();
+            nana.Acelerar();
+            nana.Frenar();
+            nana.Apagar();
+
+
+
+        }
+    }
+}
